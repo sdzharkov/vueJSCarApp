286eeb2cea5061785273a91208ce2e7e65cfb4b7
Stephan Zharkov <zharkov@localhost.localdomain>
2016-12-27 19:40:56 -0800

    first

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="Center-Container is-Inline">
+    <div class="Center-Block">
+        <state1></state1>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import state1 from './components/state1'
+
+export default {
+  name: 'app',
+  components: {
+    state1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  .Center-Container.is-Flexbox {
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-align: center;
+       -moz-box-align: center;
+       -ms-flex-align: center;
+    -webkit-align-items: center;
+            align-items: center;
+    -webkit-box-pack: center;
+       -moz-box-pack: center;
+       -ms-flex-pack: center;
+    -webkit-justify-content: center;
+            justify-content: center;
+  }
+}
+</style>
---
+++ src/components/state1.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="state1">
+    <input type="text" class="form-control" placeholder="Enter Year:" v-model="car_year">
+    <input type="text" class="form-control" placeholder="Enter Make:" v-model="car_make">
+    <input type="text" class="form-control" placeholder="Enter Model:" v-model="car_model">
+    <button> Next State </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'state1',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/*h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}*/
+</style>
